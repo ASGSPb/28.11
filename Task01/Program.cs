5997ab11320853a359174a531b8bd29efd34a48f
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-29 01:06:37 +0300

    practical task 28.11

---
+++ Task01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+int totalNegativeNumbers = 0;
+bool b = true;
+while (b == true)
+{
+    Console.Write("Хотите ввести число? Если  ДА наберите  1, если НЕТ наберите 2. Ваш ответ: ");
+
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    if ( n == 1)
+    {
+    Console.Write("Введите число: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    b = true;
+        if ( m > 0)
+        {
+            totalNegativeNumbers = totalNegativeNumbers + 1;
+        }
+    }
+    else
+    {
+        Console.Write($"Спасибо! Чисел больше 0 введено {totalNegativeNumbers} раз");
+        b = false;
+    }
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,58 @@
+﻿//  Напишите программу,
+//  которая найдёт точку пересечения двух прямых, 
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//  значения b1, k1, b2 и k2 задаются пользователем.
+//  b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+System.Console.Write("Даны две прямые заданные уравнениями: ");
+System.Console.WriteLine("y = k1 * x + b1 и y = k2 * x + b2");
+System.Console.WriteLine();
+
+System.Console.WriteLine("Введите данные: ");
+
+System.Console.Write("b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("k1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("k2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine();
+
+System.Console.WriteLine($"y = {k1} * x + {b1}; y = {k2} * x + {b2};");
+System.Console.WriteLine();
+
+double x = 0;
+double y =0;
+
+if (k1 - k2 != 0 && b2 - b1 != 0)
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    System.Console.WriteLine($"Прямые пересекаются в точке с координатами х {x}; y{y};");
+    System.Console.WriteLine();
+}
+else if (b2 - b1 == 0)
+        {
+           System.Console.WriteLine($"Прямые совпадают");
+           System.Console.WriteLine(); 
+        }
+        else if (k1 - k2 == 0)
+                {
+                  System.Console.WriteLine($"Прямые параллельны");
+                  System.Console.WriteLine();  
+                }
+
+
+
+
+
+
+
+
+

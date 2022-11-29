f6715885752581d937ff56f4a3bc3881c320e9ab
ASGSPb <118271073+ASGSPb@users.noreply.github.com>
2022-11-29 20:27:33 +0300

    добавил метод

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -28,3 +28,24 @@ while (b == true)
         b = false;
     }
 }
+
+// Console.Write("Сколько чисел вы хотите ввести? ");
+// int numb = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int [numb];
+
+// int PositiveCount(int count)
+// {
+//     int countOfPositive = 0;
+//     for(int i = 0; i < count;i++)
+//     {
+//         array[i] = Convert.ToInt32(Console.ReadLine());
+//         if (array[i] > 0)
+//         {
+//             countOfPositive+=1;
+//         }
+//         Console.WriteLine(string.Join(",", array));
+//     }
+//     return countOfPositive;
+// }
+// int posCount = PositiveCount(numb);
+// Console.Write($"количество положительных чисел = {posCount}");
